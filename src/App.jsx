7f01dc62f5cdc0c56204467b27911e1dc506c49c
Karl von der Eltz <karlvondereltz@gmail.com>
2024-03-06 18:11:28 +0100

    moved out of fullstackopen folder

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+const App = () => {
+  const words = [[tail, past, may, arm], [present, archery, hair, orchestra], [Future, Logic, Common, Drake], [leg, breast, nugget, broth]]
+}
+
+export default App;
